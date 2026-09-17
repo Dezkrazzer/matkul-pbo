98216c299c3eab0a890af541dd5b1c41ae6a9cc2
Dezkrazzer <uqie.vanmadiun@gmail.com>
2026-09-17 11:12:56 +0700

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,5 @@
-class Person {
-    private String nama;
-    private int umur;
-
-    public void setNama(String nama) {
-        this.nama = nama;
-    }
-
-    public void setUmur(int umur) {
-        if (umur > 0 && umur < 150) {
-            this.umur = umur;
-        } else {
-            System.out.println("Umur tidak valid");
-        }
-    }
-
-    public String getNama() {
-        return nama;
-    }
-
-    public int getUmur() {
-        return umur;
-    }
-}
 public class Main {
-    public static void main(String[] args) throws Exception{
-        Person p1 = new Person();
-        p1.setNama("Andi");
-        p1.setUmur(200);
-        System.out.println("Nama: " + p1.getNama() + ", Umur: " + p1.getUmur());
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
     }
 }
